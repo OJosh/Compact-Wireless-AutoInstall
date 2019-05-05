beaef7baf12cb9bb32da53a8a0964b309d20c87a
Josh <oppressings@gmail.com>
2019-05-05 12:53:15 +0100

    Output
    Changed the output to "compat-wireless-2010-10-10.tar.bz2" so the script actually works

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # make sure to run the file as sudo install.sh
-wget https://github.com/hunghtbk/compat-wireless-2.6-2010-10/blob/master/compat-wireless-2010-10-10.tar.bz2?raw=true
+wget https://github.com/hunghtbk/compat-wireless-2.6-2010-10/blob/master/compat-wireless-2010-10-10.tar.bz2?raw=true -O compat-wireless-2010-10-10.tar.bz2
 tar -jxvf compat-wireless-2010-10-10.tar.bz2
 cd compat-*
 make unload
